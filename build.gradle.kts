ebb53add677c9be5cdf32db4cd9f411fb670d8a1
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-06-08 10:43:26 +0800

    clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,9 @@ val implementationVersion: String by project
 val versionMc: String by project
 val versionForge: String by project
 
+val githubRepository: String by project
+val publishBranch: String by project
+
 val META_PROJECTS: List<String> = listOf(
     "fabric-api-bom",
     "fabric-api-catalog"
@@ -209,10 +212,20 @@ publishMods {
     dryRun.set(!providers.environmentVariable("CI").isPresent)
     displayName.set("[$versionMc] Forgified Fabric API ${project.version}")
 
-//    github {
-//        accessToken.set(providers.environmentVariable("GITHUB_TOKEN"))
-//        repository.set(githubRepository)
-//        commitish.set(publishBranch)
+    github {
+        accessToken.set(providers.environmentVariable("GITHUB_TOKEN"))
+        repository.set(githubRepository)
+        commitish.set(publishBranch)
+    }
+//    curseforge {
+//        accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
+//        projectId.set(curseForgeId)
+//        minecraftVersions.add(versionMc)
+//    }
+//    modrinth {
+//        accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
+//        projectId.set(modrinthId)
+//        minecraftVersions.add(versionMc)
 //    }
 }
 
